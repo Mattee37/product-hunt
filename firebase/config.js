2e8c8bb1788b7dcc0eb2fe81377e2b437918ae9d
Mattee37 <bluestacksmm.137@gmail.com>
2021-07-24 01:47:37 -0300

    first commit

---
+++ firebase/config.js
@@ -0,0 +1,13 @@
+// En este archivo ponemos todas las configuraciones que nos provee Firebase.
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+export default firebaseConfig;
